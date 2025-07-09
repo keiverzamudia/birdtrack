0b328408c3a3a0d713a26a4a8df458d6d06cd935
EDGARDOX69 <edgardotorrealba34f.a@gmail.com>
2025-07-09 13:31:13 -0400

    Update gestionMantenimiento.php

--- app/vista/gestionMantenimiento.php
+++ app/vista/gestionMantenimiento.php
@@ -41,10 +41,12 @@ require_once 'componentes/menu.php';
                     } ?>
                   </select>
                 </div>
-                <div class="mb-4">
-                  <label for="responsable" class="form-label">Empleado Responsable</label>
-                  <input type="text" class="form-control" id="responsable" name="responsable" required>
-                </div>
+                <select class="form-select mb-3" name="responsable" id="responsable" required>
+                  <option selected disabled>Seleccione el Responsable</option>
+                  <?php foreach ($usuarios as $usuario) {
+                    echo "<option value='" . $usuario['cedula_empleado'] . "'>" . $usuario['Nombre_Empleado'] . "</option>";
+                  } ?>
+                </select>
                 <div class="mb-4">
                   <label for="tipo" class="form-label">Tipo de Mantenimiento</label>
                   <select class="form-select mb-3" name="tipo" id="tipo" required>
@@ -109,7 +111,7 @@ require_once 'componentes/menu.php';
                     <td> <?= $mantenimiento['id_mantenimiento'] ?> </td>
                     <td> <?= $mantenimiento['nombre_activo'] ?> </td>
                     <td> <?= $mantenimiento['id_activo'] ?> </td>
-                    <td> <?= $mantenimiento['Empleado_Responsable'] ?> </td>
+                    <td> <?= $mantenimiento['nombre_empleado'] ?> </td>
                     <td> <?= $mantenimiento['tipo_mtto'] ?> </td>
                     <td> <?= $mantenimiento['Estado'] ?> </td>
                     <td> <?= $mantenimiento['Fecha'] ?> </td>
@@ -138,12 +140,11 @@ require_once 'componentes/menu.php';
                             d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
                         </svg>
                       </button>
-
-                    </td>
-                  </tr>
+                </form>
+                </td>
+                </tr>
               </tbody>
 
-              <!--modal de edicion-->
               <div class="modal fade" id="editar<?= $mantenimiento['id_mantenimiento'] ?>" data-bs-backdrop="static"
                 data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
                 <div class="modal-dialog">
@@ -157,12 +158,20 @@ require_once 'componentes/menu.php';
                     <!-- formulario de edicion a mantenimiento -->
                     <div class="modal-body">
                       <form method="POST">
-                        <input type="hidden" name="id" value="<?= $mantenimiento['id_mantenimiento'] ?>">
+                        <input type="hidden" name="id" value="<?php $mantenimiento['id_mantenimiento'] ?>">
                         <div class="mb-4">
+
                           <label for="InputEmpleado" class="form-label">Empleado Responsable</label>
-                          <input type="text" class="form-control" name='responsable' id="InputEmpleado"
-                            aria-describedby="empleadoHelp" value="<?php echo $mantenimiento['Empleado_Responsable']; ?>"
-                            required>
+                          <select class="form-control" name='responsable' id="InputEmpleado" aria-describedby="empleadoHelp"
+                            value="<?php echo $usuarios['cedula_empleado']; ?>" required>
+                            <option selected disabled><?php echo $mantenimiento['nombre_empleado'] ?>
+                              <?php foreach ($usuarios as $empleado) {
+                                echo "<option value='" . $empleado['cedula_empleado'] . "'>"
+                                  . $empleado['Nombre_Empleado'] . "</option>";
+
+                              } ?>
+                            </option>
+                          </select>
                           <div id="empleadolHelp" class="form-text">Ingrese el Nombre del Responable del
                             Activo</div>
                         </div>
@@ -248,7 +257,7 @@ require_once 'componentes/menu.php';
       $(".btnEliminar").each((index, element) => {
         $(element).on('click', (e) => {
           $('#btnEliminarMantenimiento').val($(e.target).closest('tr').find('td:eq(0)').text())
-          $('#nombreEliminacion').text($(e.target).closest('tr').find('td:eq(1)').text())
+          $('#nombreEliminacion').text($(e.target).closest('tr').find('td:eq(0)').text())
         })
       })
     });
